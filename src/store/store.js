0f749eb3322075d0d2cfc094a388fd90617a9e6e
Vasiliy Igonin <vigonin83@gmail.com>
2022-10-21 16:05:23 +0300

    create Redux & delete function

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { createStore } from "redux";
+import { taskReducer } from "./taskReducer";
+
+const initialState = [
+   { id: 1, title: 'Task1', completed: false },
+   { id: 2, title: 'Task2', completed: false }]
+
+   // console.log(initialState);
+
+export function initiateStore() {
+   return createStore(taskReducer, initialState)
+}
---
+++ src/store/taskReducer.js
@@ -0,0 +1,22 @@
+import { taskUpdated, taskDeleted } from "./actionTypes"
+
+export function taskReducer(state = [], action) {
+   switch (action.type) {
+      case taskUpdated: {
+         const newArray = [...state]
+         const elementIndex = newArray.findIndex((el) => el.id === action.payload.id)
+         newArray[elementIndex] = {
+            ...newArray[elementIndex],
+            ...action.payload
+         }
+         return newArray
+      }
+      case taskDeleted: {
+         let newArray = [...state]
+         newArray = newArray.filter((el) => el.id !== action.payload.id)
+         return newArray
+      }
+      default:
+         return state;
+   }
+}
